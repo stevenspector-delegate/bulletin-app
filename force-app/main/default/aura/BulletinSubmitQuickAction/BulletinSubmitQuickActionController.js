795f42edd2096a2340d29031b8927b5b3fdf028c
Your Name <your.github@email.com>
2025-09-23 16:34:59 -0400

    Update component metadata and permission sets, add aura directory

---
+++ force-app/main/default/aura/BulletinSubmitQuickAction/BulletinSubmitQuickActionController.js
@@ -0,0 +1,9 @@
+({
+    handleSuccess : function(component, event, helper) {
+        var toast = $A.get("e.force:showToast");
+        $A.get("e.force:closeQuickAction").fire();
+    },
+    handleCancel : function(component, event, helper) {
+        $A.get("e.force:closeQuickAction").fire();
+    }
+})
